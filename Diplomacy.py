24ba290cb6e7b6e778a8682d7a60d7493b1d5bd4
“Kevin <kevinw.austin@utexas.edu>
2020-07-16 04:47:47 -0500

    fixed bugs in Diplomacy.py

--- Diplomacy.py
+++ Diplomacy.py
@@ -6,9 +6,8 @@ class Army:
         self.power = 0
         self.fighting = False
     
-    def support(army):
-        army.power += 1
-        
+    def __str__(self):
+        return self.name + " " + self.city
 
 def diplomacy_start(s):
     info = s.split()
@@ -29,17 +28,24 @@ def diplomacy_action(armies, battlefield):
         battle = [army]
         j = i + 1
         while j < len(armies):
-            if armies[j].city = army.city:
-                battle.append(army)
+            if armies[j].city == army.city:
+                battle.append(armies[j])
                 army.fighting = True
                 armies[j].fighting = True
-        battlefield.append(battle)
+            j += 1
+        if len(battle) > 1:
+            battlefield.append(battle)
 
 
-def diplomacy_support(armies):
+def diplomacy_support(armies, results):
     for army in armies:
-        if (not army.fighting) and (not not army.target):
-            army.support(target)
+        if (not army.fighting):
+            results.append(army.name + " " + army.city)
+            if not not army.target:
+                for army2 in armies:
+                    if army2.name == army.target:
+                        army2.power += 1
+                        break
 
 
 def diplomacy_outcome(battlefield):
@@ -54,7 +60,8 @@ def diplomacy_outcome(battlefield):
                     battle.remove(army)
             max_power += 1
         result = battle[0].name + " " + battle[0].city
-    return sorted(outcome)
+        outcome.append(result)
+    return outcome
     
 
 def diplomacy_solve(r, w):
@@ -67,7 +74,8 @@ def diplomacy_solve(r, w):
         armies.append(diplomacy_start(s))
     battlefield = []
     diplomacy_action(armies, battlefield)
-    diplomacy_support(armies)
-    results = diplomacy_outcome(battlefield)
-    for army in results:
+    results = []
+    diplomacy_support(armies, results)
+    results.extend(diplomacy_outcome(battlefield))
+    for army in sorted(results):
         w.write(army + "\n")
